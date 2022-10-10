294e6b5b0c4f5fa5db00dff5ca8b94ab69c6edca
OriginalPou <chaari.mahdi@yahoo.fr>
2022-10-10 15:29:24 +0000

    lab 4 work added

--- lab4/trajectory_generation_pkg/src/trajectory_generation_node.cpp
+++ lab4/trajectory_generation_pkg/src/trajectory_generation_node.cpp
@@ -11,9 +11,17 @@
 #include <mav_trajectory_generation/polynomial_optimization_linear.h>
 #include <mav_trajectory_generation/trajectory.h>
 
+#include <tf2/utils.h>
 #include <tf/transform_datatypes.h>
+#include <tf2_eigen/tf2_eigen.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <eigen3/Eigen/Dense>
+
+#include <stdlib.h> 
+
+#include <cmath>
+#define PI M_PI
+
 
 class WaypointFollower {
   [[maybe_unused]] ros::Subscriber currentStateSub;
@@ -38,9 +46,10 @@ class WaypointFollower {
     //  Populate the variable x, which encodes the current world position of the
     //  UAV
     // ~~~~ begin solution
-    //
-    //     **** FILL IN HERE ***
-    //
+    
+    geometry_msgs::Point posX = cur_state.pose.pose.position;
+    x << posX.x, posX.y, posX.z;
+
     // ~~~~ end solution
     // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
     // ~
@@ -108,17 +117,39 @@ class WaypointFollower {
     double last_yaw = 0;
     for (auto i = 0; i < poseArray.poses.size(); ++i) {
       // Populate vertices (for the waypoint positions)
-      //
-      //
-      //     **** FILL IN HERE ***
-      //
-      //
+      
+      geometry_msgs::Point vertexPos = poseArray.poses[i].position;
+      Eigen::Vector3d pos;
+      pos << vertexPos.x, vertexPos.y, vertexPos.z;
+      
+
       // Populate yaw_vertices (for the waypoint yaw angles)
-      //
-      //
-      //     **** FILL IN HERE ***
-      //
-      //
+      
+      geometry_msgs::Quaternion YPR = poseArray.poses[i].orientation;
+      tf2::Quaternion q;
+      tf2::fromMsg(YPR, q);
+      double yaw = tf2::getYaw(q);
+      //ROS_INFO_STREAM("VERTEX YAX" << yaw);
+      yaw =  yaw>=0? yaw : yaw+2*PI;
+      ROS_INFO_STREAM("VERTEX YAW" << yaw);
+      
+      if(i< poseArray.poses.size()-1){
+        // intermidiate position
+        mav_trajectory_generation::Vertex int_position(D);
+        int_position.addConstraint(POSITION, pos);
+        vertices.push_back(int_position);
+
+        mav_trajectory_generation::Vertex int_yaw(1);
+        int_yaw.addConstraint(ORIENTATION, yaw);
+        yaw_vertices.push_back(int_yaw);
+
+      }else{
+        end_position.makeStartOrEnd(pos, SNAP);
+        vertices.push_back(end_position);
+        end_yaw.addConstraint(ORIENTATION,yaw);
+        yaw_vertices.push_back(end_yaw);
+      }
+
     }
 
     // ~~~~ end solution
@@ -134,8 +165,8 @@ class WaypointFollower {
     // HINT: play with these segment times and see if you can finish
     // the race course faster!
     std::vector<double> segment_times;
-    const double v_max = 8.0;
-    const double a_max = 4.0;
+    const double v_max = 69.0;
+    const double a_max = 34.0;
     segment_times = estimateSegmentTimes(vertices, v_max, a_max);
 
     // =====================================================
@@ -180,9 +211,9 @@ class WaypointFollower {
     //
     // ~~~~ begin solution
     trajectory_msgs::MultiDOFJointTrajectoryPoint next_point;
-    next_point.time_from_start = ros::Duration(0.0);  // <--- Correct this
-
-    double sampling_time = 0;  // <--- Correct this
+    //next_point.time_from_start = ros::Duration(0.0);  // <--- Correct this
+    next_point.time_from_start = ros::Time::now()- trajectoryStartTime;
+    double sampling_time = ros::Time::now().toSec()- trajectoryStartTime.toSec();  // <--- Correct this
     if (sampling_time > trajectory.getMaxTime())
       sampling_time = trajectory.getMaxTime();
 
@@ -197,13 +228,52 @@ class WaypointFollower {
     ROS_INFO("Traversed %f percent of the trajectory.",
              sampling_time / trajectory.getMaxTime() * 100);
 
+    ROS_INFO_STREAM("des_orientation" << des_orientation);
+    double des_Yaw = des_orientation[0];
+    //des_Yaw = std::fmod(des_Yaw+ 2*PI, 2*PI);
+    /*if (des_Yaw > PI || des_Yaw < -PI){
+      des_Yaw = des_orientation[0]>=0? std::fmod(des_orientation[0], PI) : -std::fmod(abs(des_orientation[0]), PI);
+    }*/
+    
+    ROS_INFO_STREAM("des_yaw" << des_Yaw);   
     // Populate next_point
-    //
-    //
-    //     **** FILL IN HERE ***
-    //
-    //
+    geometry_msgs::Vector3 posX;
+    posX.x = des_position(0);
+    posX.y = des_position(1);
+    posX.z = des_position(2);
+    
+    geometry_msgs::Quaternion ori ;
+    tf2::Quaternion myQ;
+    myQ.setRPY(0, 0, des_Yaw);
+    ori = tf2::toMsg(myQ);
 
+    geometry_msgs::Transform desiredVertex;
+    desiredVertex.rotation = ori;
+    desiredVertex.translation = posX;
+
+    geometry_msgs::Twist velocity;
+    velocity.linear.x = des_velocity(0);
+    velocity.linear.y = des_velocity(1);
+    velocity.linear.z = des_velocity(2);
+
+    velocity.angular.x = velocity.angular.y = velocity.angular.z = 0;
+    geometry_msgs::Twist acceleration;
+    acceleration.linear.x = des_accel(0);
+    acceleration.linear.y = des_accel(1);
+    acceleration.linear.z = des_accel(2);
+    acceleration.angular.x = acceleration.angular.y = acceleration.angular.z = 0;
+    
+    next_point.transforms.resize(1);
+    next_point.velocities.resize(1);
+    next_point.accelerations.resize(1);
+
+    next_point.transforms[0]=desiredVertex;
+    next_point.velocities[0]=velocity;
+    next_point.accelerations[0]=acceleration;
+
+
+    //publishing the desired MultiDOFJointTrajectoryPoint
+    desiredStatePub.publish(next_point);
     // ~~~~ end solution
     // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
     // ~
